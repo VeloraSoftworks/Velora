44f268c21e1ae153f39c483629f94f7b8dfc4b8d
Ollie0000 <132388201+Ollie0000@users.noreply.github.com>
2025-03-04 17:45:57 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,6 @@
 local Games = {
-    [527730528] = 'https://raw.githubusercontent.com/VeloraSoftworks/Velora/refs/heads/main/Games/magic%20training.lua'
+    [527730528] = 'https://raw.githubusercontent.com/VeloraSoftworks/Velora/refs/heads/main/Games/magic%20training.lua',
+    [1067808551] = 'https://raw.githubusercontent.com/VeloraSoftworks/Velora/refs/heads/main/Games/wizard%20life.lua'
 }
 
 if not game:IsLoaded() then
