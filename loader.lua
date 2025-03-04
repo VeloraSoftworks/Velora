ca8ab83a056a1159d2328ece36be845a9a6ad9d0
Ollie0000 <132388201+Ollie0000@users.noreply.github.com>
2025-03-04 17:50:00 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
-local Games = {
-    [527730528] = 'https://raw.githubusercontent.com/VeloraSoftworks/Velora/refs/heads/main/Games/magic%20training.lua',
+local Scripts = {
+    [527730528] = 'https://raw.githubusercontent.com/VeloraSoftworks/Velora/refs/heads/main/Games/magic%20training.lua'
     [1067808551] = 'https://raw.githubusercontent.com/VeloraSoftworks/Velora/refs/heads/main/Games/wizard%20life.lua'
 }
 
@@ -7,8 +7,23 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-if Games[game.PlaceId] then
-    local Loaded = loadstring(game:HttpGet(Games[Game.PlaceId]))
+local function Notify(Description)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+        Title = "Velora",
+        Text = Description
+    })
+end
 
-    pcall(Loaded)
+if Scripts[game.PlaceId] then
+    Notify("Loading, may take a while...")
+    local success, result = pcall(function()
+        return loadstring(game:HttpGet(Scripts[Game.PlaceId]))
+    end)
+
+    if not success then
+        Notify("An error occured, you can check error in console.")
+        warn("Velora error: " .. result)
+    end
+else
+    Notify("No available scripts for this game.")    
 end
